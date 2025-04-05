2e1293c68f6b6f88dccb20fd48fb4639e8944164
GitJohnFis <jfisiiahi69@gmail.com>
2025-04-05 04:11:55 +0000

    Change  client landing page path to the correct html file (socket.html)

--- Server/server.js
+++ Server/server.js
@@ -1,46 +1,48 @@
+//<!-- This code is imprtant and responsibvle for importing libraries -->
 
 //UPDATED server.js v1.0.0
-<!-- This code is imprtant and responsibvle for importing libraries-->
-const express = request("express");// repsponsible for senfing files
-const http = require("http");//responsible for connecting to the internet
+// This code is imprtant and responsibvle for importing libraries
+const express = require("express");// repsponsible for senfing files
+const http = rquire("http");//responsible for connecting to the internet
 const socketIo = require("socket.io");//responsible for sending anf receiving real time date(text and data)
 const path = require("path");//responible for finding and navigating file paths
-const app = express();intitiating the express library
+const app = express(); //intitiating the express library
 const server = http.createServer(app); //creating server that sends files
 const io = socketIo(server); //making socket.io responsible for the real-time connection logic
 
 
 //Means that anyone that enters pur website, we sent him the index.html first and they start seeing our page
 
-const clientLandingPagePath = path.join(__dirname, "../", "/client/index.html");
+const clientLandingPagePath = path.join(__dirname, "../",
+    "/Client/socket.html");
 app.get("/", (req, res) => {
-res.sendfileclientLandingPagePath);
+    res.sendFile(clientLandingPagePath);
 });
 
 //starting the server on port 2008, we can have multiple servers on multiple ports - localhost:2000
-server.listen(2000, () => {
-console.log("Sever is up on: 2000");//print anything for test or whatevs
+server.listen(2008, () => {
+    console.log("Sever is up on: 2008");//print anything for test or whatevs
 });
 
 let players = {}; //this is where we store the information for all the connected players
 
 //The socket.oi connection logic (This is going to make the game look fluid or run smooth)
 io.on("connection", (socket) => {
-//Code for the moment someone connects
-console.log("a user connected")
-  player[socket.io] = { y: 0, y: 0]; // we created a player and stored it in the player list
+    //Code for the moment someone connects
+    console.log("a user connected")
 
+    //Code for the moment someone disconnects
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+});
 
-  //Whenever recieve move command from the client
-socket.on("move", (data)->
-         players[socket.id].x += data.dx; // move in the x-axis using the received data
-         players[socket.id].y += data.dy; //move in the y-axis using the received data
-          });
-//Code for the moment someone disconnects
-socket.on("disconnect", () => {
-console.log("user disconnected");
-});
-});
+//Our tick function
+let framerate = 30;
+setInteraval(function () {
+    io.emit("state", "players"); // send the list of the players and all their info to everyone
+
+}, 1000 / framerate);
 
 //in the TERMINAL input this text = npm install express
 
