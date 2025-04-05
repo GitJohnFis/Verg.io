3bd59ee639d1ecd20d61ad3d98b133eae524d7d2
GitJohnFis <jfisiiahi69@gmail.com>
2025-04-05 14:39:56 +1300

    Include movement commands from client in X and Y axis in server.js
    Send movements commands from client in (X, Y) axis to the server.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+//UPDATED server.js v1.0.0
 <!-- This code is imprtant and responsibvle for importing libraries-->
 const express = request("express");// repsponsible for senfing files
 const http = require("http");//responsible for connecting to the internet
@@ -8,10 +10,9 @@ const server = http.createServer(app); //creating server that sends files
 const io = socketIo(server); //making socket.io responsible for the real-time connection logic
 
 
-//Means that anyone that enters pur website, we sent him the index.html
-first and they start seeing our page
+//Means that anyone that enters pur website, we sent him the index.html first and they start seeing our page
 
-cosnt clientLandingPagePath = path.join(__dirname, "../", "/client/index.html");
+const clientLandingPagePath = path.join(__dirname, "../", "/client/index.html");
 app.get("/", (req, res) => {
 res.sendfileclientLandingPagePath);
 });
@@ -21,11 +22,20 @@ server.listen(2000, () => {
 console.log("Sever is up on: 2000");//print anything for test or whatevs
 });
 
+let players = {}; //this is where we store the information for all the connected players
+
 //The socket.oi connection logic (This is going to make the game look fluid or run smooth)
 io.on("connection", (socket) => {
 //Code for the moment someone connects
 console.log("a user connected")
+  player[socket.io] = { y: 0, y: 0]; // we created a player and stored it in the player list
 
+
+  //Whenever recieve move command from the client
+socket.on("move", (data)->
+         players[socket.id].x += data.dx; // move in the x-axis using the received data
+         players[socket.id].y += data.dy; //move in the y-axis using the received data
+          });
 //Code for the moment someone disconnects
 socket.on("disconnect", () => {
 console.log("user disconnected");
